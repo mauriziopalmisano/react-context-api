69967e50ace3f68d684b2d3955a82af335729690
mauriziopalmisano <maurizio.palmisano1@gmail.com>
2026-05-14 17:20:02 +0200

    creazione del bottone per la budget mode

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,16 @@
 import { NavLink } from 'react-router-dom'
+import useBudget from '../hooks/useBudget';
 
 
 
 function Navbar() {
 
     const handleActive = ({ isActive }) => isActive ? "nav-link text-white my-active" : "text-white nav-link";
+    const {budgetMode, setBudgetMode} = useBudget();
+    const btnLabel = budgetMode ? 'Disattiva Modalità Budget' : 'Attiva Modalità Budget';
+    const btnClass = budgetMode ? 'btn-danger' : 'btn-success';
+    const budgetModeToggle = () => (setBudgetMode(!budgetMode));
+
 
 
     return (
@@ -15,6 +21,7 @@ function Navbar() {
                     <NavLink className={handleActive} to='/'>Home</NavLink>
                     <NavLink className={handleActive} to='chi_siamo'>Chi Siamo</NavLink>
                     <NavLink className={handleActive} to='prodotti'>Prodotti</NavLink>
+                    <button className={`btn ${btnClass}`} onClick={budgetModeToggle}>{btnLabel}</button>
                 </div>
             </div>
         </nav>
